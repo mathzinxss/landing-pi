a8b199ae6a3fb0248f7b3802617c8bbd66dd7568
mathznxs.dev <math983839179@gmail.com>
2025-01-29 08:47:06 -0300

    1

---
+++ src/sections/Header.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { ArrowRight } from "lucide-react";
+import { useState } from "react";
+import logo from '@/assets/logosaas.png'
+import Image from 'next/image'
+import { Menu, X } from "lucide-react"
+
+const menu = [
+  { id: 1, name: 'Sobre', link: '#' },
+  { id: 2, name: 'Características', link: '#' },
+  { id: 3, name: 'Clientes', link: '#' },
+  { id: 4, name: 'Atualizações', link: '#' },
+  { id: 5, name: 'Ajuda', link: '#' },
+]
+
+export const Header = () => {
+
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen)
+  }
+
+  return (
+    <header className="sticky top-0 z-50 backgrop-blur-lg border-b border-neutral-700/80">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden lg:flex">Simplifique seu fluxo de trabalho e aumente sua produtividade</p>
+        <div className="inline-flex gap-1 items-center">
+          <p>Começe agora</p>
+          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
+        </div>
+      </div>
+      <div className='container p-3 mx-auto relative lg:text-sm'>
+        <div className='flex justify-between items-center'>
+          <div className='flex items-center flex-shrink-0'>
+            <Image className='h-10 w-10 mr-2 mb-3' src={logo} alt="logo" />
+            <a href='#' className='text-xl tracking-tight hidden lg:flex'>Aretis</a>
+          </div>
+          <ul className="hidden lg:flex gap-4">
+            {menu.map((data) => (
+              <li key={data.id}>
+                <a href={data.link}>{data.name}</a>
+              </li>
+            ))}
+          </ul>
+          <button className="hidden lg:flex justify-center bg-black text-white rounded-lg font-medium p-2 tracking-tight">Obtenha de graça</button>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button
+              onClick={toggleNavbar}
+              className="relative w-8 h-8 flex items-center justify-center">
+              <Menu className={`absolute transition-all duration-300 ease-in-out transform ${mobileDrawerOpen ? "opacity-0 scale-50 rotate-90" : "opacity-100 scale-100 rotate-0"}`} />
+              <X className={`absolute transition-all duration-300 ease-in-out transform ${mobileDrawerOpen ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-50 rotate-90"}`} />
+            </button>
+          </div>
+        </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {menu.map((data) => (
+                <li key={data.id} className='py-4'>
+                  <a href={data.link}>{data.name}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6 mt-2">
+              <a href="#" className="py-2 px-3 border rounded-md">Entrar</a>
+              <a href="#" className=" py-2 px-3 rounded-md bg-black text-white">Obtenha Agora</a>
+            </div>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
