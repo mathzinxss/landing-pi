602fb344155778bbd3709d43645abe059f591124
mathznxs.dev <math983839179@gmail.com>
2025-01-29 23:04:51 -0300

    1

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,24 @@
+import acmeLogo from '@/assets/logo-acme.png'
+import quantumLogo from '@/assets/logo-quantum.png'
+import echoLogo from '@/assets/logo-echo.png'
+import apexLogo from '@/assets/logo-apex.png'
+import celestialLogo from '@/assets/logo-celestial.png'
+import pulseLogo from '@/assets/logo-pulse.png'
+import Image from 'next/image'
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div>
+      <div className="container">
+        <div className='flex gap-14 flex-none'>
+          <Image src={acmeLogo} alt=''/>
+          <Image src={quantumLogo} alt=''/>
+          <Image src={apexLogo} alt=''/>
+          <Image src={celestialLogo} alt=''/>
+          <Image src={echoLogo} alt=''/>
+          <Image src={pulseLogo} alt=''/>
+        </div>
+      </div>
+    </div>
+  );
 };
