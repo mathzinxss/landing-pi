0b4b6672b00c33e1c0c801ab2777a3d54f8ad635
mathznxs.dev <math983839179@gmail.com>
2025-01-30 16:54:42 -0300

    1

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -56,11 +56,11 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section className="py-24">
+    <section className="py-24 overflow-hidden">
       <div className="container">
         <h2 className="section-title ">Planos</h2>
         <p className="section-description mt-5">Livre para sempre. Atualize para tarefas ilimitadas, melhor segurança e recursos exclusivos.</p>
-        <div className='flex items-center justify-between mt-16'>
+        <div className='flex flex-col gap-8 items-center mt-14 lg:flex-row'>
           {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features }) => (
             <div key={title} className='p-10 border border-black/15 rounded-3xl shadow-[0_7px_14px_#EAEAEA]'>
               <h3 className='text-lg font-bold text-black/50'>{title}</h3>
@@ -68,9 +68,9 @@ export const Pricing = () => {
                 <span className='text-4xl font-bold tracking-tighter'>R${monthlyPrice}</span>
                 <span>/mês</span>
               </div>
-              <ul className='flex flex-col gap-5 mt-4'>
+              <ul className='flex flex-col gap-5 mt-8'>
                 {features.map((feature, index) => (
-                  <li key={index} className="flex items-center gap-2">
+                  <li key={index} className="text-sm flex items-center gap-4">
                     <CheckImage className='h-6 w-6' />
                     <span>{feature}</span>
                   </li>
